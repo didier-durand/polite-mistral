29dc15d18a1f07b19da580bf90bd4846c6649e38
Didier Durand <didduran@amazon.ch>
2024-06-29 08:56:13 +0200

    v0.5

--- genai_model.py
+++ genai_model.py
@@ -6,7 +6,8 @@ class GenAiModel(Enum):
     MISTRAL_7B = "open-mistral-7b"
     MISTRAL_TINY = "mistral-tiny"
     MISTRAL_EMBED = "mistral-embed"
-    MISTRAL_FINE_TUNED = "ft:open-mistral-7b:48d56ecd:20240627:b9ce4e1d"
+    # fine-tuned model with 60K samples over 60 steps (June 28th)
+    MISTRAL_FINE_TUNED = "ft:open-mistral-7b:48d56ecd:20240628:e31fa6b7"
     # Hugging Face
     HF_MISTRAL_7B = "huggingface-llm-mistral-7b"
     HF_GEMMA_7B = "huggingface-llm-gemma-7b-instruct"
